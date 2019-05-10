728148dac544134d70a2ec9aaed1e64b4f08b959
fradleybrancis <fradleybrancis@gmail.com>
2019-05-09 21:19:53 -0700

    update crud routes

--- server/controllers.js
+++ server/controllers.js
@@ -1,27 +1,48 @@
 const { Review, sequelize } = require("../database/sequelize");
 
 
-exports.get = (req, res) => {
+exports.getAll = (req, res) => {
   Review.findAll({
       where: {
           home_id: req.params.abode_id
       }
-  }).then((reviews) => {
-      res.send(reviews);
+  })
+  .then((reviews) => {
+      res.status(200).send(reviews);
+  })
+  .catch(error => {
+      res.status(500).send("Error : ", error)
   })
 };
 
-// ${req.params.abode_id}
-
-// exports.get = (req, res) => {
-//   sequelize.query(`SELECT * FROM reviews WHERE home_id = ${req.params.abode_id};`)
-//   .then((reviews) => {
-//     res.send(reviews);
-//   })
-// };
-
 exports.addReview = (req, res) => {
-    res.send("POST recieved");
-    // Review.create(req)
-    // res.end()
+    Review.findOne({ where: req.body.id })
+      .then((obj) => {
+          if (obj) {
+            obj.update(req.body);
+            res.status(200).send("Review Updated");
+          } else {
+            Review.create(req.body);
+            res.status(200).send("Review Created");
+          }
+      })
+      .catch(error => {
+          res.status(500).send(console.log(error));
+      })
+}
+
+exports.deleteReview = (req, res) => {
+    id = req.params.id;
+    Review.destroy({
+        where: { id: id }
+    })
+    .then(response => {
+        if (response === 1) {
+            console.log("Review Deleted");
+        }
+        res.sendStatus(200)
+    })
+    .catch(error => {
+        res.status(500).send(console.log(error));
+    })
 }
--- server/index.js
+++ server/index.js
@@ -1,29 +1,23 @@
-require('newrelic');
+// require('newrelic');
 const express = require('express');
 const path = require('path');
-// const compression = require('compression');
-// const controller = require("./controllers");
-const { Review, sequelize } = require("../database/sequelize");
+const controllers = require("./controllers");
+
 
 const app = express();
 const port = process.env.PORT || 3002;
 
-// app.use(compression());
+app.use(express.json());
+
 app.use(express.static('public'));
 
-app.get('/abodes/:abode_id/reviews', (req, res) => {
-    Review.findAll({
-        where: {
-            home_id: req.params.abode_id
-        }
-    }).then((reviews) => {
-        res.send(reviews);
-    })
-    // sequelize.query("SELECT * FROM reviews WHERE id = " + req.params.abode_id)
-    // .then(reviews => res.send(reviews))
-});
+app.get('/abodes/:abode_id/reviews', controllers.getAll);
 
-// app.post('/abodes/:abode_id/reviews', controller.addReview);
+app.post('/abodes', controllers.addReview);
+
+app.put("/abodes", controllers.addReview);
+
+app.delete("/abodes/:id/reviews", controllers.deleteReview);
 
 
 // app.get('/*', (req, res) => {
